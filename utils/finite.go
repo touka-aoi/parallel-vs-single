d033615677c71cbb26c4c245a7f1addf858c0934
touka-aoi <toukaseisei@gmail.com>
2025-11-11 23:31:39 +0900

    fix: validateはそれぞれが持つ

---
+++ utils/finite.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"math"
+
+	"github.com/touka-aoi/paralle-vs-single/application/domain"
+)
+
+func FiniteVec(v domain.Vec2) bool {
+	return !(isFinite(v.X) || isFinite(v.Y))
+}
+
+func isFinite(f float64) bool {
+	return !math.IsNaN(f) && !math.IsInf(f, 0)
+}
